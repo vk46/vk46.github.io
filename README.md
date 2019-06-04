ea2f3bdefb324a02aec6c02e3b1a345a108f4917
Vinay Kumar <vk.hack46@gmail.com>
2019-06-04 14:44:44 +0530

    Implemented From Data Submit Function, Smooth Scroll Error Resolved
    Implemented from data submit function in index html and js files, Smooth Scroll error resolved.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ### VK46
-Portfolio to showcase professional and personal information - [vk46.tech](https://vk46.github.io/)
+Portfolio to showcase professional and personal information - [vk46.github.io](https://vk46.github.io/)
 
 ### Built With
 
 * [Bootstrap](https://getbootstrap.com/) - Front-End Framework
 * [jQuery](https://jquery.com/) - JavaScript Library
-* [GitHub Pages](https://pages.github.com/) - Hosted directly from your GitHub repository
+* [GitHub Pages](https://pages.github.com/) - Hosted directly from GitHub repository
 
 ### Thanks To
 
--- script/index.js
+++ script/index.js
@@ -20,3 +20,21 @@ jQuery(document).ready(function($) {
     $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
   });
 });
+//submit contact form data
+function submitFormData() {
+  var myObject = new Object();
+  myObject.name = $("#name").val();
+  myObject.email = $("#email").val();
+  myObject.phone = $("#name1").val();
+  myObject.message = $("#message").val();
+  if (myObject.name == "") {
+    alert("Please enter name");
+  }else if (myObject.email == "") {
+    alert("Please enter email");
+  }else if (myObject.phone == "") {
+    alert("Please enter phone");
+  }else {
+    alert("Thank you for contact me, will get back to you in sometime!");
+    console.log(myObject);
+  }
+}
