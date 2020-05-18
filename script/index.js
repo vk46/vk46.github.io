5e73da95c179cde57f532643e94e1de5baafc1dd
vinay kumar <vk.hack46@gmail.com>
2020-05-18 12:48:46 +0530

    changes done in index page, stats added, experience changes done

--- script/index.js
+++ script/index.js
@@ -1,8 +1,6 @@
 jQuery(document).ready(function($) {
-  $("#gauge1").gauge(90,{color: "#ff4f81",unit: " %",type: "halfcircle"});
-  $("#gauge2").gauge(70, {color: "#2dde98", unit: " %",type: "halfcircle"});
-  $("#gauge3").gauge(75, {color: "#00aeff",unit: " %",type: "halfcircle"});
-  $("#gauge4").gauge(40, {color: "#ffb900",unit: " %",type: "halfcircle"});
+  $("#gauge1").gauge(80,{color: "#ff4f81",unit: " %",type: "halfcircle"});
+  $("#gauge2").gauge(40, {color: "#2dde98", unit: " %",type: "halfcircle"});
   (function() {
     $.fatNav();
   }());
