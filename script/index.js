fa54d76cba64b0e9900c681a3c76dd29e4f7aae8
vinay kumar <vk.hack46@gmail.com>
2020-06-06 12:08:52 +0530

    changes done in index html and js files

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
   $("#gauge1").gauge(70,{color: "#ff4f81",unit: " %",type: "halfcircle"});
-  $("#gauge2").gauge(60, {color: "#4d90fe", unit: " %",type: "halfcircle"});
+  $("#gauge2").gauge(50, {color: "#4d90fe", unit: " %",type: "halfcircle"});
   $("#gauge3").gauge(30, {color: "#2dde98", unit: " %",type: "halfcircle"});
   (function() {
     $.fatNav();
